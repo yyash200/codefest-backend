47a397e0692ee4bf11a256deda173f623b545288
yyash200 <yyash200@gmail.com>
2022-09-28 16:55:03 +0530

    Initial commit

---
+++ src/main/java/com/startyounguk/codefestbackend/service/dto/AboutUs.java
@@ -0,0 +1,15 @@
+package com.startyounguk.codefestbackend.service.dto;
+
+public class AboutUs {
+
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
